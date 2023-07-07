d28543a8b2aef0f676c94d10881084d9e30af219
jenboc <47900748+jenboc@users.noreply.github.com>
2023-07-07 20:28:32 +0100

    Ball spawner

--- Assets/Scripts/BallSpawnerController.cs
+++ Assets/Scripts/BallSpawnerController.cs
@@ -4,15 +4,91 @@ using UnityEngine;
 
 public class BallSpawnerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _ball;
+    [SerializeField] private float spawnCooldown;
+    [SerializeField] private float minSpawnCooldown;
+    [SerializeField] private float spawnCooldownDelta;
+
+    private float _cooldownTimer;
+    private Camera _camera;
+
+    private const float SPAWN_RADIUS = 10;
+    
+    private void Start()
     {
+        _cooldownTimer = 0f;
+        _camera = Camera.main;
+    }
+    
+    private void Update()
+    {
+        _cooldownTimer += Time.deltaTime;
         
+        if (_cooldownTimer >= spawnCooldown)
+        {
+            SpawnBall();
+            ReduceCooldown();
+            _cooldownTimer = 0f;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ReduceCooldown()
     {
+        spawnCooldown -= spawnCooldownDelta;
         
+        if (spawnCooldown < minSpawnCooldown)
+        {
+            spawnCooldown = minSpawnCooldown;
+            spawnCooldownDelta = 0;
+        }
+    }
+
+    private void SpawnBall()
+    {
+        var spawnPos = GetSpawnPosition();
+        spawnPos.z = 0; // Ensure the ball is visible on screen
+        Instantiate(_ball, spawnPos, Quaternion.identity);
+    }
+
+    private Vector3 GetSpawnPosition()
+    {
+        var border = Random.value;
+
+        // Left Border
+        if (border <= 0.25)
+        {
+            return _camera.ScreenToWorldPoint(new Vector3(
+                -SPAWN_RADIUS,
+                Random.Range(-SPAWN_RADIUS, _camera.pixelHeight + SPAWN_RADIUS),
+                0
+            ));
+        }
+        
+        // Top Border
+        if (border <= 0.5)
+        {
+            return _camera.ScreenToWorldPoint(new Vector3(
+                Random.Range(-SPAWN_RADIUS, _camera.pixelWidth + SPAWN_RADIUS),
+                -SPAWN_RADIUS,
+                0
+            ));
+        }
+        
+        // Right Border
+        if (border <= 0.75)
+        {
+            return _camera.ScreenToWorldPoint(new Vector3(
+                _camera.pixelWidth + SPAWN_RADIUS,
+                Random.Range(-SPAWN_RADIUS, _camera.pixelHeight + SPAWN_RADIUS),
+                0
+            ));
+        }
+        
+        // Bottom Border
+        return _camera.ScreenToWorldPoint(new Vector3(
+            Random.Range(-SPAWN_RADIUS, _camera.pixelWidth + SPAWN_RADIUS),
+            _camera.pixelHeight + SPAWN_RADIUS,
+            0
+        ));
     }
 }
